b42f97a993d5200b2503d82671eab8c9b73be9eb
fennewald <c@rson.riker.me>
2022-05-26 04:30:16 -0400

    added flakes

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A tiny tool for process killing";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          murder = pkgs.buildGoModule {
+            pname = "murder";
+            inherit version;
+            src = ./.;
+            vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
+        });
+      defaultPackage = forAllSystems (system: self.packages.${system}.murder);
+    };
+}
